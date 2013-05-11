f438aed6db9a72b4e25bbae72740e23b82891ec3
Alexander Chernyakhovsky <achernya@mit.edu>
2013-05-11 21:38:58 +0000

    In athrun:   * Merge debathena-gathrun and debathena-attachandrun into the debathena-athrun source package.   * Rewrite debathena-attachandrun in Python.     - Fix a segementation fault in attachandrun that could be triggered by       running with just "-c" and a locker.     - Stop trying to run the file as a shell script if execv() fails.   * Switch to DEP-5-style copyright file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='attachandrun',
+      version='10.2',
+      description='Athena utility for scripts to run a program from a locker',
+      author='Alexander Chernyakhovsky',
+      scripts=['attachandrun']
+      )
